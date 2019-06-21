79ed9e3c51fb6defcdfb90691496cddbc8b0e35d
Roman <merlkori2015@gmail.com>
2019-06-21 13:58:40 +0300

    small fix

--- index.js
+++ index.js
@@ -3,34 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const createDB = require('./create-db');
 
-const wordsDB = createDB('db/words');
-
-// const Datastore = require('nedb');
-// const db = new Datastore({filename : 'db/words', autoload: true});
-// const db_user = new Datastore({filename : 'db/user', autoload: true});
-
-// function promisifyDB (fn, context) {
-// 	return (...args) => {
-// 		return new Promise((resolve, reject) => {
-// 			fn.call(context, ...args, (error, data) => {
-// 				if (error) return reject(error);
-// 				resolve(data);
-// 			});
-// 		});
-// 	}
-// }
-
-
-// const insert = promisifyDB(db.insert, db);
-// insert({name : "Boris the Bladeaaaaaa", year: 1946});
-
-// wordsDB.create({name : "Borissss the Bladeaaaaaa", year: 1941})
-//   .then(data => console.log(data))
-//   .catch(err => console.log(err));
-/* Add */
-// const cl = (err, data)=> console.log(data);
-// db.insert({name : "Boris the Bladeasdasd", year: 1946}, cl);
-// get 
+const wordsDB = createDB('db/words'); 
 
 app.listen(PORT, function () {
   console.log(`Server listening on port ${PORT}!`);
